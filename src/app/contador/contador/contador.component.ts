066a320c4bda7704b71d53c26af46da33e234217
mhernandez04 <mauricio.hernandez@figment.com.mx>
2021-12-13 13:00:16 -0600

    primer commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    template: `
+        <h1> {{ titulo }}</h1>
+        <h3>La base es: <strong> {{ base }}</strong></h3>
+
+        <button (click)=" adicion( -base ) "> -{{ base }} </button>
+        <span> {{ numero }} </span>
+        <button (click)=" adicion( base ) "> +{{ base }} </button>
+    `
+})
+
+export class ContadorComponent {
+    public titulo: string = 'Contador App';
+    public numero: number = 10;
+    public base: number = 5;
+  
+    adicion(num: number){
+      this.numero += num 
+    }
+}
